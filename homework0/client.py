5b7ba57e4121c92c8c81d011aecb2c518e1ed8e5
Macintosh <bitiutskii.ad@phystech.edu>
2024-08-06 11:09:19 +0200

    ok_check

--- homework0/client.py
+++ homework0/client.py
@@ -6,4 +6,8 @@ PORT = 8080
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     data = s.recv(1024)
-    print(f"Received {data!r}")
+
+    if data.decode("utf-8") == "OK":
+        print(f"Recieved right data! -- {data.decode('utf-8')}")
+    else:
+        print(f"Recieved wrong data! -- {data.decode('utf-8')}")
